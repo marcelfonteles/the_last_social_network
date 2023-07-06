409238c15ff36804859f4da872c3741acc25bb1b
marcelfonteles <marcelfonteles@gmail.com>
2023-07-06 21:33:56 +0000

    begin to write tests for users_controller

--- spec/requests/api/v1/users_spec.rb
+++ spec/requests/api/v1/users_spec.rb
@@ -1,7 +1,51 @@
 require 'rails_helper'
 
 RSpec.describe "Api::V1::Users", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  describe "GET /api/v1/users" do
+    before do
+      FactoryBot.create_list(:user, 20)
+    end
+
+    it 'returns all users from page 1' do
+      get '/api/v1/users'
+      expect(response).to have_http_status(:ok)
+      expect(JSON.parse(response.body).size).to eq(10)
+    end
+
+    it 'returns all users from page 2' do
+      get '/api/v1/users', params: { page: 2, hits_per_page: 10 }
+      expect(response).to have_http_status(:ok)
+      expect(JSON.parse(response.body).size).to eq(10)
+    end
+
+    it 'returns empty array of users' do
+      get '/api/v1/users', params: { page: 10, hits_per_page: 10 }
+      expect(response).to have_http_status(:ok)
+      expect(JSON.parse(response.body).size).to eq(0)
+    end
   end
+
+  describe "GET /api/v1/users/:id" do
+    let (:user) { create(:user) }
+
+    it 'returns user' do
+      get "/api/v1/users/#{user.id}"
+
+      expect(response).to have_http_status(:ok)
+      response_user = JSON.parse(response.body).deep_symbolize_keys!
+      expect(response_user[:id]).to eq(user.id)
+    end
+
+    it 'returns an error' do
+      get "/api/v1/users/999"
+
+      expect(response).to have_http_status(:unprocessable_entity)
+      res = JSON.parse(response.body).deep_symbolize_keys!
+      expect(res[:message]).to eq("User not found.")
+    end
+
+  end
+
+  # describe "POST /api/v1/users"
+  # describe "PUT /api/v1/users"
 end
