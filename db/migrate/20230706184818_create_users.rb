ea725a5242636106c4fa6b8fe02987b4764c8328
marcelfonteles <marcelfonteles@gmail.com>
2023-07-06 19:35:24 +0000

    create users model tests

--- db/migrate/20230706184818_create_users.rb
+++ db/migrate/20230706184818_create_users.rb
@@ -3,7 +3,7 @@ class CreateUsers < ActiveRecord::Migration[7.0]
     create_table :users do |t|
       t.string :name
       t.date :birthday
-      t.int :gender
+      t.integer :gender
       t.decimal :last_latitude, precision: 15, scale: 10
       t.decimal :last_longitude, precision: 15, scale: 10
 
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    it { is_expected.to validate_presence_of(:name) }
+    it { is_expected.to validate_presence_of(:birthday) }
+    it { is_expected.to validate_presence_of(:gender) }
+    it { is_expected.to validate_numericality_of(:last_latitude).is_less_than_or_equal_to(90.0) }
+    it { is_expected.to validate_numericality_of(:last_latitude).is_greater_than_or_equal_to(0.0) }
+    it { is_expected.to validate_numericality_of(:last_longitude).is_less_than_or_equal_to(90.0) }
+    it { is_expected.to validate_numericality_of(:last_longitude).is_greater_than_or_equal_to(0.0) }
+  end
+
+  describe 'enums' do
+    it { should define_enum_for(:gender) }
+    it { should define_enum_for(:gender).with_values(%w[male female transgender questioning not_applicable]) }
+  end
+
+  # describe 'relationships' do
+  #   it { is_expected.to have_many(:inventory_items) }
+  # end
 end
