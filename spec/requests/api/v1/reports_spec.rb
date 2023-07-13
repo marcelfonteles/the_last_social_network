a49b9ddab33a33347fa4e219f8cf9884d69d6167
marcelfonteles <marcelfonteles@gmail.com>
2023-07-13 22:12:59 +0000

    add tests for controller and for interactor

--- spec/requests/api/v1/reports_spec.rb
+++ spec/requests/api/v1/reports_spec.rb
@@ -1,7 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe "Api::V1::Reports", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  describe "GET /api/v1/reports" do
+    before do
+      10.times do
+        user = create(:user)
+        create(:inventory, user: user, item: 'water')
+        create(:inventory, user: user, item: 'food')
+        create(:inventory, user: user, item: 'medicine')
+        create(:inventory, user: user, item: 'ammo')
+      end
+
+      get("/api/v1/reports")
+    end
+
+    subject(:res) { JSON.parse(response.body).deep_symbolize_keys }
+
+    it 'returns success' do
+      expect(response).to have_http_status(:ok)
+    end
+
+    it 'expect to have an object in body' do
+      expect(res).to be_a_kind_of(Hash)
+    end
   end
 end
